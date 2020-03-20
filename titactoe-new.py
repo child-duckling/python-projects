5ce5e03c09ee199b6e26422d802a2d1c9d76ea15
child_duckling <samrjeffs@gmail.com>
2020-03-20 15:31:57 -0700

    Update titactoe-new.py

--- titactoe-new.py
+++ titactoe-new.py
@@ -2,9 +2,9 @@ from turtle import *
 from tkinter import *
 from math import *
 l = 50
+ht()
 
-
-
+region = [ 11 , 12 , 13 , 21 , 22 , 23 , 31 , 32 , 33 ]
 
 
 def lineto(x1, y1, x2, y2):
@@ -12,13 +12,12 @@ def lineto(x1, y1, x2, y2):
     goto(x1,y1)
     pendown()
     goto(x2,y2)
-
-
-
+    
 def boardCreate():
     lineto(-0.5 * l, 1.5 * l, -0.5 * l, -1.5 * l)
     lineto(0.5 * l, 1.5 * l, 0.5 * l, -1.5 * l)
+    lineto(-1.5 * l, -0.5 * l, 1.5 * l, -0.5 * l)
     lineto(1.5 * l, 0.5 * l, -1.5 * l, 0.5 * l)
-    lineto(1.5 * l, 0.5 * l, 1.5 * l, 0.5 * l)
+    
 
 boardCreate()
