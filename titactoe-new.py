7ca77f11b0c25f8b7df0c989cec62b2145769df0
child_duckling <samrjeffs@gmail.com>
2020-04-03 19:02:10 -0700

    Error thrown if outside of box

--- titactoe-new.py
+++ titactoe-new.py
@@ -4,8 +4,22 @@ from math import *
 l = 50
 #ht()
 
+
+
+
+
+
+
+
+
+
+
+
+
+p = ""
+regions = {0:"nA", 1:"nA", 2:"nA", 3:"nA", 4:"nA", 5:"nA", 6:"nA", 7:"nA", 8:"nA"}
 squareLocations = [[-l, l], []]
-name[squareLocations][0]
+#name[squareLocations][0]
 win = False
 def lineto(x1, y1, x2, y2):
     penup()
@@ -18,30 +32,30 @@ def boardCreate():
     lineto(0.5 * l, 1.5 * l, 0.5 * l, -1.5 * l)
     lineto(-1.5 * l, -0.5 * l, 1.5 * l, -0.5 * l)
     lineto(1.5 * l, 0.5 * l, -1.5 * l, 0.5 * l)
-def centerFinder():
+#def centerFinder():
     
 def reigonFinder(x,y):
     penup()
-    if x < -l / 2:
-        if y > l / 2:
+    if x < -l / 2 and x > -l / 2 - l :
+        if y > l / 2 and y < l / 2 + l:
             return 0
-        elif y < -l / 2:
+        elif y < -l / 2 and y > -l / 2 - l:
             return 6
-        else:
+        elif y > -l/2 and y < l / 2:
             return 3
-    elif x > l / 2:
-        if y > l / 2:
+    elif x > l / 2 and x < l / 2 + l:
+        if y > l / 2 and y < l / 2 + l:
             return 2
-        elif y < -l / 2:
+        elif y < -l / 2 and y > -l / 2 - l:
             return 8
-        else:
+        elif y > -l/2 and y < l / 2:
             return 5
-    else:
-        if y > l / 2:
+    elif x < l/2 and x > -l/2:
+        if y > l / 2 and y < l / 2 + l:
             return 1
-        elif y < -l / 2:
+        elif y < -l / 2 and y > -l / 2 - l:
             return 7
-        else:
+        elif y > -l/2 and y < l / 2:
             return 4
     
     
@@ -50,7 +64,12 @@ def reigonFinder(x,y):
 def clickLocation(x,y):
     goto(x,y)
     reigon = reigonFinder(x,y)
-def checkReigon():
+    #print(reigon)
+    checkReigon(reigon)
+def checkReigon(a):
+    if regions[a] == '':
+        regions[a] = "x"
+        print( regions )
     
 
 
