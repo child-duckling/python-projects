6c35b989faf01eab6a75393538c8a24985d6db61
child_duckling <19170969+child-duckling@users.noreply.github.com>
2020-05-29 18:58:50 -0700

    Finishe :/

--- titactoe-new.py
+++ titactoe-new.py
@@ -7,7 +7,7 @@ l = 25
 ht()
 speed(0)
 person = True #True - x ; False - o
-regions = {10:"nA", 11:"nA", 12:"nA", 20:"nA", 21:"nA", 22:"nA", 30:"nA", 31:"nA", 32:"nA"}
+regions = {10:"_", 11:"_", 12:"_", 20:"_", 21:"_", 22:"_", 30:"_", 31:"_", 32:"_"}
 squareCenter =[
     ( -l ,l - (l / 2) ),
     ( 0 , l - (l / 2) ),
@@ -22,8 +22,28 @@ squareCenter =[
 rowcol = [10,11,12,20,21,22,30,31,32]
 info = 0, 0 - l * 5
 #isFirstMove = True
-
-
+def convertModToReg(x):
+    
+    if x == 10:
+        return 0
+    elif x == 11:
+        return 1
+    elif x == 12:
+        return 2
+    elif x == 20:
+        return 3
+    elif x == 21:
+        return 4
+    elif x == 22:
+        return 5
+    elif x == 30:
+        return 6
+    elif x == 31:
+        return 7
+    elif x == 32:
+        return 8
+    else:
+        return
 def lineto(x1, y1, x2, y2):
     penup()
     goto(x1,y1)
@@ -58,7 +78,7 @@ def reigonFinder(x,y):
         elif y < -l / 2 and y > -l / 2 - l:
             return 31
         elif y > -l/2 and y < l / 2:
-            return 41
+            return 21
     else:
         print("Not on the board! \n ")
         return None
@@ -71,28 +91,35 @@ def clickLocation(x,y):
     
     goto(x,y)
     region = reigonFinder(x,y)
-    checkBoardResult = checkBoard(region)
+    regRegion = convertModToReg(region)
+    
     if region != None:
-        if checkBoardResult == "no" or checkBoardResult == "nA"  :  
+
+        if regions[region] == "_" :  
             if  checkReigon(region) == True:
+                
                 if person == True:
-                    drawX(squareCenter[region])
-                    regions.update(region="X")
+                    drawX(squareCenter[regRegion])
+                    regions[region] = "x"
                 else:
-                    drawO(squareCenter[region])
-                    regions.update(region="O")
+                    drawO(squareCenter[regRegion])
+                    regions[region] = "o"
                 person = not person
-        else: #Ending Sequence
-            penup()
-            goto(info)
-            write(checkBoardResult + " has won the game")
-            sety(ycor() - l)
-            write("Click The Board To Exit")
-            print(checkBoardResult + " has won the game \n Good Game! \n\n\n\n\nClick The Board To Exit")
-            onscreenclick(gameOver)
-            return     
+                if checkBoard(region) == "x" or checkBoard(region) == "o":#Ending Sequence
+                    penup()
+                    goto(info)
+                    write(checkBoard(region) + " has won the game")
+                    sety(ycor() - l)
+                    write("Click the board To exit")
+                    print(checkBoard(region) + " has won the game \n Good Game! \n\n\n\n\nClick The Board To Exit")
+                    onscreenclick(gameOver)
+                    return
+            else:
+                print("\nTry Again!")
+        else: 
+            print("\nTry Again!")
     else:
-        print("Try again!")
+        print("\nTry again!")
 
 def drawO(square):
     goto(square)
@@ -131,7 +158,7 @@ def printInfo(t):
 
 def checkReigon(r):
     global regions
-    if regions[r] == "nA":
+    if regions[r] == "_":
         return True
     print("Try Again")
     return False
@@ -141,24 +168,23 @@ def checkReigon(r):
 def checkBoard(x):
     
     row1 = [regions[x] for x in rowcol if x // 10 == 1]
-    row2 = [regions[x] for x in rowcol if x // 20 == 1]
+    row2 = [regions[x] for x in rowcol if x > 19 and x < 23 ]
     row3 = [regions[x] for x in rowcol if x // 30 == 1]
-    column1 = [regions[x] for x in rowcol if x % 10 == 1]
-    column2 = [regions[x] for x in rowcol if x % 10 == 2]
-    column3 = [regions[x] for x in rowcol if x % 10 == 3]
-    for i in row1:
-        print(i)
-    for i in row2:
-        print(i)
-    for i in row3:
-        print(i)
+    column1 = [regions[x] for x in rowcol if x % 10 == 0]
+    column2 = [regions[x] for x in rowcol if x % 10 == 1]
+    column3 = [regions[x] for x in rowcol if x % 10 == 2]
+    print(len(row2))
+    print("\nChecking Board....")
+    print(row1)
+    print(row2)
+    print(row3)
 
     
     for i in [row1,row2,row3]:
-        if i[0] == i[1] == i [2]:
+        if i[0] == i[1] == i[2]:
             return i[1]
     for i in [column1,column2,column3]:
-        if i[0] == i[1] == i [0]:
+        if i[0] == i[1] == i[2]:
             return i[1]
     
     
@@ -174,8 +200,8 @@ def checkBoard(x):
         return "no"
 
 def gameOver(x,y): #Game Closer, gets set as the function that gets run in onscreenclick()
-    print("\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n") #"Clearing" The Screen
-    exit(2010001110110000101101101011001010010000001000011011011000110111101110011011001010110010000100001) #Game Closed in Binary
+    print("\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n") #"Clearing" The Output
+    exit(1010001110110000101101101011001010010000001000011011011000110111101110011011001010110010000100001) #Game Closed in Bi_ry
 
 boardCreate()
 penup()
